c5a2db16ff242d8ca2700dbbefb2d2339bd7dd8b
decaplanet <126875757+decaplanet@users.noreply.github.com>
2023-07-28 23:16:20 +0900

    Use a Constant for Output Messages

--- rust/rock-paper-scissors.rs
+++ rust/rock-paper-scissors.rs
@@ -1,6 +1,12 @@
 use rand::Rng;
 use std::io::stdin;
 
+const RESULTS: [[&str; 3]; 3] = [
+    ["It's a draw", "You lose", "You win"],
+    ["You win", "It's a draw", "You lose"],
+    ["You lose", "You win", "It's a draw"],
+];
+
 fn to_ascii_art<'a>(input: usize) -> &'a str {
     match input {
         0 => {
@@ -39,7 +45,7 @@ _______
     }
 }
 
-fn rock_paper_scissors(results: &[&[&str; 3]]) {
+fn rock_paper_scissors() {
     println!("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. :");
 
     let mut user_selection = String::new();
@@ -51,24 +57,18 @@ fn rock_paper_scissors(results: &[&[&str; 3]]) {
     if user_selection < 3 {
         println!("{}", to_ascii_art(user_selection));
         println!("Your computer chose:\n{}", to_ascii_art(computer_selection));
-        let result = results[user_selection][computer_selection];
+        let result = RESULTS[user_selection][computer_selection];
         println!("{}", result);
 
         if result == "It's a draw" {
-            rock_paper_scissors(results);
+            rock_paper_scissors();
         }
     } else {
         println!("Your input is not valid. Please enter again.");
-        rock_paper_scissors(results);
+        rock_paper_scissors();
     }
 }
 
 fn main() {
-    let results: &[&[&str; 3]] = &[
-        &["It's a draw", "You lose", "You win"],
-        &["You win", "It's a draw", "You lose"],
-        &["You lose", "You win", "It's a draw"],
-    ];
-
-    rock_paper_scissors(results);
+    rock_paper_scissors();
 }
